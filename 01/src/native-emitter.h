8b74c33c436cf37943830d293968fae979653401
NickNaso <nicoladelgobbo@gmail.com>
2018-04-04 00:57:34 +0200

    Check and fix 2nd example

--- 01/src/native-emitter.h
+++ 01/src/native-emitter.h
@@ -21,7 +21,9 @@ class NativeEmitter : public Napi::ObjectWrap<NativeEmitter> {
     public:
         static Napi::Object Init(Napi::Env env, Napi::Object exports);
         NativeEmitter(const Napi::CallbackInfo& info);
-        Napi::Value CallAndEmit(const Napi::CallbackInfo& info);
+        
     private:
         static Napi::FunctionReference constructor;
+
+        Napi::Value CallAndEmit(const Napi::CallbackInfo& info);
 };
